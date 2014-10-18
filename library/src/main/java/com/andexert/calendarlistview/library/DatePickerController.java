88aeb9025cbe4ec9edcf902f97176cceff2c1b58
Monkeyrash <salzanoo6256@hwdsb.on.ca>
2014-10-17 23:21:16 -0400

    Update DatePickerController.java

--- library/src/main/java/com/andexert/calendarlistview/library/DatePickerController.java
+++ library/src/main/java/com/andexert/calendarlistview/library/DatePickerController.java
@@ -25,6 +25,6 @@ package com.andexert.calendarlistview.library;
 
 public interface DatePickerController {
 	public abstract int getMaxYear();
-
+  	public abstract int getMinYear();
 	public abstract void onDayOfMonthSelected(int year, int month, int day);
 }
